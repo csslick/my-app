99e7ec870057190cc7e32a680fe5842bafd3ba65
csslick <digitone@naver.com>
2023-04-03 16:45:10 +0900

    첫 업로드
    상세 설명

---
+++ index.js
@@ -0,0 +1,10 @@
+// add module
+const { format } = require('date-fns');
+
+var d = new Date();
+console.log(d);
+// 포맷변환: 4자리수 년도-2자리수 월-2자리수 일 
+var formatedD = format(d, 'yyyy-MM-dd');
+console.log(formatedD);
+
+ 
